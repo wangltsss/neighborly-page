99639009b2ca5caf88e825b1151f75ff98a3b353
Hao Li <lihao.kuma@gmail.com>
2026-01-20 22:16:22 -0500

    add(client): add global NativeWind style

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import '../../global.css';
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
